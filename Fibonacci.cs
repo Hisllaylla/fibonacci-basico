a952882e76c98673fa38a0a74c34dafa92e9a7a3
Hisllaylla <hisllayllak@gmail.com>
2023-09-16 21:57:47 -0300

    first commit

---
+++ Fibonacci.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace FibonacciBasico
+{
+    public class Fibonacci
+    {
+        public int[] CalcularFibonacci()
+        {
+            int NumeroUm = 1;
+            int NumeroDois = 0;
+            int Soma = 0;
+
+            int[] Sequencia = new int[21];
+
+            for (int i = 0; i <= 20; i++)
+            {
+                Sequencia[i] = NumeroDois; // nº atual
+                Soma = NumeroUm + NumeroDois;
+                NumeroDois = NumeroUm;
+                NumeroUm = Soma;
+            }
+            return Sequencia; // retorna array contendo a sequência
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FibonacciBasico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci SequenciaFibonacci = new Fibonacci();
+            int[] SF = SequenciaFibonacci.CalcularFibonacci();
+
+            Console.WriteLine("A sequência de Fibonacci é: " + string.Join(", ", SF));
+        }
+    }
+}
